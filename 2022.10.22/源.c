55fbaddfb868b2bd4ec0755ff0adfacb890c3c7e
Sange66 <116428160+Sange66@users.noreply.github.com>
2022-10-22 16:40:01 +0800

    Add files via upload

---
+++ 2022.10.22/源.c
@@ -0,0 +1,16 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<studio.h>
+
+int main()
+{
+	int a, b;
+	scanf("%d%d", &a, &b);
+	if (a > b)
+		printf("%d\n", a);
+	else
+		printf("%d\n", b);
+		
+
+
+	return 0;
+}
