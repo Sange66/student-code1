429aa43afe16b92cd9b4e1aa282a2c521c923093
Sange <116428160+Sange66@users.noreply.github.com>
2022-10-26 11:01:35 +0800

    Add files via upload

---
+++ 2022.10.26/源.c
@@ -0,0 +1,18 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+int main()
+{
+	    int a;
+		printf("觉得作者帅的扣1，反之扣2");
+		scanf("%d", &a);
+		switch (a)
+		{
+		case 1:
+				printf("谢谢");
+				break;
+		case 2:
+				printf("sb");
+
+		}
+
+}
