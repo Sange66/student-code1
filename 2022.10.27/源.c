7ff8bb356e268866108805bbee4215208336728c
Sange <116428160+Sange66@users.noreply.github.com>
2022-10-27 21:24:03 +0800

    Add files via upload

---
+++ 2022.10.27/源.c
@@ -0,0 +1,42 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+//int main()
+//{
+//	int i = 0;
+//	while (i < 10)
+//	{
+//		printf("%d ", i);
+//		i++;
+//	}
+//
+//
+//	return 0;
+//}
+
+
+
+//int main()
+//{
+//	int i = 0;
+//for (i = 0; i < 10; i++)
+//{
+//	printf("%d ", i);
+//}
+//
+//
+//	return 0;
+//}
+
+
+int main()
+{
+	int i = 0;
+	do
+	{
+		printf("%d ", i);
+		i++;
+	} while (i < 10);
+
+
+	return 0;
+}
