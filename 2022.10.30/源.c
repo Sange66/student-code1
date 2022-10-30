af2b40d4b1ff420a35a2b4cb07e7965738029e3d
Sange <116428160+Sange66@users.noreply.github.com>
2022-10-30 22:56:32 +0800

    Add files via upload

---
+++ 2022.10.30/源.c
@@ -0,0 +1,16 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+int main()
+{
+	int a, b, c, d;
+	scanf("%d%d%d", &a, &b, &c);
+	if (a < b)
+		d = a, a = b, b = d;
+	if (a < c)
+		d = a, a = c, c = d;
+	if (b < c)
+		d = b, b = c, c = d;
+	printf("%d %d %d", a, b, c);
+
+	return 0;
+}
