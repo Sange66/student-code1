e8e977f89ce1f399b2865d51fda6692fcddba6f1
Sange <116428160+Sange66@users.noreply.github.com>
2022-11-28 17:33:41 +0800

    Add files via upload

---
+++ 2022.11.28/源.c
@@ -0,0 +1,17 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+int main()
+{
+	int arr[10] = { 1,2,3,4,5,6,7,8,9,10 },i,a;
+	a = arr[0];
+	for (i = 0; i <9; i++)
+	{
+		arr[i] = arr[i+1];
+	}
+	arr[9] = a;
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d ", arr[i]);
+	}
+	return 0;
+}
